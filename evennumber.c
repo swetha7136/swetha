42707abaf45e752249fd78e988a65bb1a73633eb
swetha7136 <35727826+swetha7136@users.noreply.github.com>
2018-02-09 16:05:05 +0530

    Create evennumber.c

---
+++ evennumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,i;
+    printf("enter the value for a,b");
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+    }
+}
+
